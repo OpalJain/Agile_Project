04c4e313f368e66f33df9d49c9d59e9988212941
Opal Jain <a00315938@student.tus.ie>
2024-03-23 21:06:07 +0530

    add comments in HelloWorld file to solve duplication issue in SonarQube

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//print Hello World!
 		 System.out.println("Hello World!");
+		//print How are you?
 		 System.out.println("How are you?");
 	}
 
