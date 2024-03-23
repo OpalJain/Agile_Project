8eaad1a4f8e6b0c2b08f35bda849e84d030ce05f
Opal Jain <a00315938@student.tus.ie>
2024-03-24 05:21:15 +0530

    Added a print statement "I am good" in my HelloWorld.java file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		 System.out.println("Hello World!");
 		//print how are you?
 		 System.out.println("How are you?");
+		//print i am good!
+		 System.out.println("I am good!");
 	}
 
 }
