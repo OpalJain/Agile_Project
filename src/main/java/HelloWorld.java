11e4d24ce18aaad2bb5735119f95a830b896f87a
Opal Jain <a00315938@student.tus.ie>
2024-03-23 21:22:01 +0530

    Removed Comments in HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,9 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		//print Hello World!
 		 System.out.println("Hello World!");
-		//print How are you?
 		 System.out.println("How are you?");
 	}
 
